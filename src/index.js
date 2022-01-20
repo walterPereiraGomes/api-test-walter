cbfb4ed3f6daa5186a61e61cad3fc661950e907a
Walter Pereira <walterpereira@deltainova.com.br>
2022-01-20 12:52:52 -0300

    configure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 var express = require("express");
 const app = express();
-const purchasesRoutes = require('../src/handlers/purchases')
+// const purchasesRoutes = require('../src/handlers/purchases')
 
-app.use(purchasesRoutes);
+// app.use(purchasesRoutes);
+
+app.get('/', (req, res) => {
+    res.send('David Gay!');
+})
 
 const port = process.env.PORT || 3000;
 
