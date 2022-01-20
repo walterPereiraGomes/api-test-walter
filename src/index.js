df185a62441d3062bc6249b115ef710fa90903ea
Walter Pereira <walterpereira@deltainova.com.br>
2022-01-20 11:54:03 -0300

    test heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const MerchandiseService = require('./services/merchandise')
 async function main() {
-    const merchandiseService = new MerchandiseService()
-
-    const list = await merchandiseService.readMerchans()
-    console.log('list :>> ', list);
+    return 'opaa';
 }
 
 main()
