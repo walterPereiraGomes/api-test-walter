a9fa0362dcb514b9c85c854906231feffb4022f6
Walter Pereira <walterpereira@deltainova.com.br>
2022-01-20 13:14:59 -0300

    ajust

--- src/handlers/purchases.js
+++ src/handlers/purchases.js
@@ -2,7 +2,7 @@ var express = require('express');
 var purchasesRoutes = express.Router();
 
 purchasesRoutes.get('/', (req, res) => {
-    res.send('initial test!');
+    res.send('initial test2!');
 })
 
 purchasesRoutes.get('/welcome', (req, res) => {
