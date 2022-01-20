5b12a50574175f530066cd76dea42800cfd36f76
Walter Pereira <walterpereira@deltainova.com.br>
2022-01-20 13:12:58 -0300

    testing automatic deploy

--- src/handlers/purchases.js
+++ src/handlers/purchases.js
@@ -2,11 +2,11 @@ var express = require('express');
 var purchasesRoutes = express.Router();
 
 purchasesRoutes.get('/', (req, res) => {
-    res.send('David Gay!');
+    res.send('initial test!');
 })
 
 purchasesRoutes.get('/welcome', (req, res) => {
-    res.send('David Gay!');
+    res.send('test welcome!');
 })
 
 module.exports = purchasesRoutes;
